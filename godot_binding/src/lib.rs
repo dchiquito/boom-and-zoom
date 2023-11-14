6d6ebcb6311e522834573c67d52de83cc1871528
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-11-14 10:22:58 -0500

    Clean up godot binding

--- godot_binding/src/lib.rs
+++ godot_binding/src/lib.rs
@@ -1,6 +1,5 @@
-use baz_core::{Game, Piece};
+use baz_core::Game;
 use baz_players::{GoFasterHeuristic, HeuristicPlayer};
-use core::ops::Deref;
 use godot::engine::{Node, NodeVirtual};
 use godot::prelude::*;
 
@@ -13,34 +12,16 @@ unsafe impl ExtensionLibrary for MyExtension {}
 #[class(base=Node)]
 struct GodotGameBoard {
     #[base]
-    base: Base<Node>,
+    _base: Base<Node>,
 
     game: Game<HeuristicPlayer<GoFasterHeuristic, i8>, HeuristicPlayer<GoFasterHeuristic, i8>>,
-    // pieces: Vec<Gd<GamePiece>>,
 }
 
 #[godot_api]
 impl NodeVirtual for GodotGameBoard {
-    fn init(mut base: Base<Node>) -> Self {
-        godot_print!("Hello, world!"); // Prints to the Godot console
-
+    fn init(base: Base<Node>) -> Self {
         let game = Game::new(GoFasterHeuristic::player(1), GoFasterHeuristic::player(1));
-
-        // let game_piece_scene: Gd<PackedScene> = load("res://GamePiece.tscn");
-        // let pieces: Vec<Gd<GamePiece>> = game
-        //     .board()
-        //     .pieces
-        //     .iter()
-        //     .map(|p| {
-        //         let mut piece = game_piece_scene.instantiate_as::<GamePiece>();
-        //         piece
-        //     })
-        //     .collect();
-        // for p in pieces {
-        //     base.add_child(p);
-        // }
-
-        Self { base, game }
+        Self { _base: base, game }
     }
 }
 
@@ -82,39 +63,3 @@ impl GodotGameBoard {
     }
 }
 
-// #[derive(GodotClass)]
-// #[class(base=Node2D)]
-// struct GamePiece {
-//     #[base]
-//     base: Base<Node2D>,
-//
-//     #[var]
-//     is_white: bool,
-//
-//     #[var]
-//     height: i64,
-//
-//     #[var]
-//     board_coords: Vector2,
-// }
-//
-// #[godot_api]
-// impl Node2DVirtual for GamePiece {
-//     fn init(base: Base<Node2D>) -> Self {
-//         Self { base, is_white:true, height:3, board_coords:Vector2{x:0.0,y:0.0} }
-//     }
-//     // #[func]
-//     fn on_notification(&mut self, piece: &Piece) {
-//         self.is_white = piece.color == baz_core::Color::White;
-//         self.height = Into::<i8>::into(&piece.height).into();
-//         self.board_coords = Vector2 {
-//             x: piece.position.x().into(),
-//             y: piece.position.y().into(),
-//         }
-//     }
-//
-// }
-//
-// #[godot_api]
-// impl GamePiece {
-// }
