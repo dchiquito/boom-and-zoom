f632e72f3e163948bc08eb331fc4ad96618f9b89
Daniel Chiquito <daniel.chiquito@gmail.com>
2024-01-04 22:25:04 -0500

    Wire protocol for dueler games

---
+++ dueler/src/lib.rs
@@ -0,0 +1,70 @@
+use baz_core::{Board, Color, GamePlayer, Move};
+
+pub fn serialize_move(mov: &Move) -> String {
+    match mov {
+        Move::Boom(index) => format!("Boom {index}"),
+        Move::Zoom(index, pos) => format!("Zoom {} {}", index, i8::from(*pos)),
+        Move::Score(index) => format!("Score {index}"),
+    }
+}
+pub fn deserialize_move(line: &str) -> Move {
+    if let Some(remainder) = line.strip_prefix("Boom ") {
+        Move::Boom(remainder.parse().expect("Invalid index"))
+    } else if let Some(remainder) = line.strip_prefix("Zoom ") {
+        let (index, pos) = remainder
+            .split_once(' ')
+            .expect("Malformed zoom serialization");
+        Move::Zoom(
+            index.parse().expect("Invalid index"),
+            pos.parse::<i8>().expect("Invalid position").into(),
+        )
+    } else if let Some(remainder) = line.strip_prefix("Score ") {
+        Move::Score(remainder.parse().expect("Invalid index"))
+    } else {
+        panic!("Unable to deserialize {line}")
+    }
+}
+
+pub struct StdioGamePlayer<T>
+where
+    T: GamePlayer,
+{
+    player: T,
+}
+
+impl<T> StdioGamePlayer<T>
+where
+    T: GamePlayer,
+{
+    pub fn new(player: T) -> StdioGamePlayer<T> {
+        StdioGamePlayer { player }
+    }
+    pub fn main(&mut self) -> std::io::Result<()> {
+        let stdin = std::io::stdin();
+        let mut buffer = String::new();
+        let mut board = Board::default();
+        // Get the color from stdin
+        stdin.read_line(&mut buffer)?;
+        let color: Color;
+        // We must make the first move if we are playing white
+        if buffer == "white" {
+            color = Color::White;
+            let our_move = self.player.decide(&board, &color);
+            board = board.apply_move(&our_move);
+            println!("{}", serialize_move(&our_move));
+        } else {
+            color = Color::Black;
+        }
+        while board.winner().is_none() {
+            // Get the opponents move from stdin and apply it to the board
+            stdin.read_line(&mut buffer)?;
+            let their_move = deserialize_move(&buffer);
+            board = board.apply_move(&their_move);
+            // Decide on a move and print it
+            let our_move = self.player.decide(&board, &color);
+            board = board.apply_move(&our_move);
+            println!("{}", serialize_move(&our_move));
+        }
+        Ok(())
+    }
+}
--- dueler/src/main.rs
+++ dueler/src/main.rs
@@ -1,7 +1,10 @@
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufRead, BufReader, Write};
 use std::path::{Path, PathBuf};
+use std::process::{ChildStdin, ChildStdout};
 
+use baz_core::{Board, Color};
+use baz_dueler::deserialize_move;
 use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
 
@@ -11,14 +14,29 @@ struct Config {
     tournament: Vec<String>,
 }
 
+impl Config {
+    fn player(&self, player_name: &str) -> &PlayerInfo {
+        self.players
+            .iter()
+            .find(|pi| pi.name == player_name)
+            .unwrap_or_else(|| panic!("No player named {player_name}"))
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 struct PlayerInfo {
     name: String,
-    args: String,
+    args: Vec<String>,
     #[serde(flatten)]
     artifact_location: ArtifactLocation,
     build: String,
     artifact: PathBuf,
+}
+
+impl PlayerInfo {
+    fn artifact_path(&self) -> PathBuf {
+        PathBuf::from("players").join(&self.name)
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -35,6 +53,12 @@ fn load_config(_args: &Args) -> Config {
     serde_yaml::from_reader(reader).expect("Failed to read config file")
 }
 
+fn update_artifact_if_necessary(player: &PlayerInfo) {
+    if !player.artifact_path().exists() {
+        update_artifact(player);
+    }
+}
+
 fn update_artifact(player: &PlayerInfo) {
     match &player.artifact_location {
         ArtifactLocation::LocalArtifact { workdir } => build_and_copy(player, workdir),
@@ -43,7 +67,6 @@ fn update_artifact(player: &PlayerInfo) {
 }
 
 fn update_git_artifact(player: &PlayerInfo, repo: &str, target: &str) {
-    std::fs::create_dir_all("git").expect("Failed to create git dir");
     let repo_dir = PathBuf::from(format!("git/{}", player.name));
     if !repo_dir.exists()
         && !std::process::Command::new("git")
@@ -77,10 +100,8 @@ fn update_git_artifact(player: &PlayerInfo, repo: &str, target: &str) {
 }
 
 fn build_and_copy(player: &PlayerInfo, build_dir: &Path) {
-    // Create the players directory if it does not exist
-    std::fs::create_dir_all("players").expect("Failed to create git dir");
     let original_dir = std::env::current_dir().expect("Failed to get the current dir");
-    let player_file = original_dir.join("players").join(&player.name);
+    let player_file = original_dir.join(player.artifact_path());
     // Navigate to the build directory
     std::env::set_current_dir(build_dir).expect("Failed to navigate to the build directory");
     // Run the build script
@@ -100,6 +121,56 @@ fn build_and_copy(player: &PlayerInfo, build_dir: &Path) {
         .expect("Failed to navigate back to the working directory");
 }
 
+fn play_match(config: &Config, white_player_name: &str, black_player_name: &str) {
+    let white_player = config.player(white_player_name);
+    let black_player = config.player(black_player_name);
+    update_artifact_if_necessary(white_player);
+    update_artifact_if_necessary(black_player);
+    let mut white_process = std::process::Command::new(white_player.artifact_path())
+        .args(&white_player.args)
+        .spawn()
+        .expect("Failed to start white player");
+    let mut white_stdin = white_process.stdin.take().unwrap();
+    let mut white_stdout = white_process.stdout.take().unwrap();
+    let mut black_process = std::process::Command::new(black_player.artifact_path())
+        .args(&white_player.args)
+        .spawn()
+        .expect("Failed to start white player");
+    let mut black_stdin = black_process.stdin.take().unwrap();
+    let mut black_stdout = black_process.stdout.take().unwrap();
+    white_stdin
+        .write_all("white\n".as_bytes())
+        .expect("Failed to write to white process");
+    black_stdin
+        .write_all("black\n".as_bytes())
+        .expect("Failed to write to black process");
+    let mut board = Board::default();
+    let mut current_color = Color::White;
+    while board.winner().is_none() {
+        board = match current_color {
+            Color::White => play_turn(&board, &mut white_stdout, &mut black_stdin),
+            Color::Black => play_turn(&board, &mut black_stdout, &mut white_stdin),
+        };
+        current_color = current_color.invert();
+    }
+    println!("Winner! {:?}", board.winner());
+}
+
+fn play_turn(board: &Board, stdout: &mut ChildStdout, stdin: &mut ChildStdin) -> Board {
+    let mut buffer = String::new();
+    let mut reader = BufReader::new(stdout);
+    reader
+        .read_line(&mut buffer)
+        .expect("Failed to read move from player");
+    println!("{buffer}");
+    let mov = deserialize_move(&buffer);
+    // TODO verify move is legal
+    stdin
+        .write_all(buffer.as_bytes())
+        .expect("Failed to send move to player");
+    board.apply_move(&mov)
+}
+
 #[derive(Parser, Debug)]
 #[command(author, version)]
 struct Args {
@@ -114,6 +185,9 @@ enum Commands {
 }
 
 fn main() {
+    // Create the git and players directories if they does not exist
+    std::fs::create_dir_all("git").expect("Failed to create git dir");
+    std::fs::create_dir_all("players").expect("Failed to create players dir");
     let args = Args::parse();
     let config = load_config(&args);
     match args.command {
@@ -122,7 +196,13 @@ fn main() {
                 update_artifact(player);
             }
         }
-        Commands::Play => todo!(),
+        Commands::Play => {
+            for white_player_name in config.tournament.iter() {
+                for black_player_name in config.tournament.iter() {
+                    play_match(&config, white_player_name, black_player_name);
+                }
+            }
+        }
     }
     println!("{config:?}");
 }
